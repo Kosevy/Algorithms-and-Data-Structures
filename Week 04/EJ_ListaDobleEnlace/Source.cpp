db2be72c2057ce2c70f01e84ad047b8ded4848af
Kosevy <112732786+Kosevy@users.noreply.github.com>
2025-07-21 11:43:45 -0500

    Add files via upload

---
+++ Week 04/EJ_ListaDobleEnlace/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "lista.h"
+#include "punto.h"
+
+using namespace std;
+
+int main() {
+	Lista<Punto*> pLista;
+	pLista.insertarInicio(new Punto(8, 1));
+	pLista.insertarInicio(new Punto(7, 2));
+	pLista.insertarInicio(new Punto(1, 3));
+	pLista.insertarInicio(new Punto(9, 4));
+	pLista.insertarInicio(new Punto(4, 5));
+	pLista.mostrar();
+	cin.get();
+	return 0;
+}
